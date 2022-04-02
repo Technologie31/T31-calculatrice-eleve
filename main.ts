e821abc515b387b10cd788039e2503c88f8ba839
Technologie31 <78386526+Technologie31@users.noreply.github.com>
2022-04-02 16:11:40 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,50 @@
+input.onButtonPressed(Button.A, function () {
+    nombre1 += 1
+    basic.showNumber(nombre1)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    basic.showNumber(nombre1 + nombre2)
+    basic.pause(5000)
+    nombre1 = 0
+    nombre2 = 0
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # # # # #
+        . . # . .
+        . . # . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    nombre2 += 1
+    basic.showNumber(nombre2)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    nombre1 = 0
+    nombre2 = 0
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # # # # #
+        . . # . .
+        . . # . .
+        `)
+})
+let nombre2 = 0
+let nombre1 = 0
+nombre1 = 0
+nombre2 = 0
+basic.showLeds(`
+    . . # . .
+    . . # . .
+    # # # # #
+    . . # . .
+    . . # . .
+    `)
