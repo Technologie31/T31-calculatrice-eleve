6e62c0850b2f658f25c4be9a1e425840c142f53e
Technologie31 <78386526+Technologie31@users.noreply.github.com>
2022-04-02 19:31:19 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -26,16 +26,6 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
         . . . . .
         `)
     basic.showNumber(nombre1 + nombre2)
-    basic.pause(500)
-    nombre1 = 0
-    nombre2 = 0
-    basic.showLeds(`
-        . . # . .
-        . . # . .
-        # # # # #
-        . . # . .
-        . . # . .
-        `)
 })
 let nombre2 = 0
 let nombre1 = 0
